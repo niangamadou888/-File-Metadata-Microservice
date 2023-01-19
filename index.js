36d3748380ed3988e6b58c9cf0a9e6de9844ef4d
niangamadou888 <71797475+niangamadou888@users.noreply.github.com>
2023-01-19 13:37:19 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config();
+const multer = require('multer');
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+const upload = multer({ dest: "./public/data/uploads"});
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+//post
+app.post('/api/fileanalyse', upload.single("upfile"), (req, res) =>{
+  const {originalname, mimetype, size } = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size,
+  });
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
